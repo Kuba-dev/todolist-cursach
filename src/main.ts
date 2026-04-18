bc36211d768c9ac91b38d09bf931c2be5b9a3075
Andrey Lebedev <andrey.lebedev@modsen-software.com>
2026-04-18 23:32:27 +0300

    refactor: refactoring the code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import * as cookieParser from "cookie-parser"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
 	app.use(cookieParser())
+
+	const config = new DocumentBuilder()
+		.setTitle("API Swagger")
+		.setDescription("TODO Swagger")
+		.setVersion("1.0")
+		.addTag("TODO")
+		.build()
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup("api", app, documentFactory)
 
 	await app.listen(3000)
 }
